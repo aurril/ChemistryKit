24d703ef412612413c7e32028c865b7e6d494ad3
Silvio Wehner <silvio.wehner@googlemail.com>
2025-01-05 10:38:29 +0100

    Initial commit

---
+++ src/client/java/aurril/chemistrykit/ChemistryKitDataGenerator.java
@@ -0,0 +1,11 @@
+package aurril.chemistrykit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChemistryKitDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
