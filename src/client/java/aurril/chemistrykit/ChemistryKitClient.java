c9cad70fbb47a532dacc66e7cc309eaf6d993c75
Silvio Wehner <silvio.wehner@googlemail.com>
2025-01-19 21:52:03 +0100

    Add remaining sulfur blocks

--- src/client/java/aurril/chemistrykit/ChemistryKitClient.java
+++ src/client/java/aurril/chemistrykit/ChemistryKitClient.java
@@ -1,10 +1,17 @@
 package aurril.chemistrykit;
 
+import aurril.chemistrykit.registry.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class ChemistryKitClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SULFUR_CLUSTER, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SMALL_SULFUR_BUD, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MEDIUM_SULFUR_BUD, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LARGE_SULFUR_BUD, RenderLayer.getCutout());
 	}
 }
--- src/main/java/aurril/chemistrykit/registry/ModBlocks.java
+++ src/main/java/aurril/chemistrykit/registry/ModBlocks.java
@@ -1,8 +1,12 @@
 package aurril.chemistrykit.registry;
 
 import aurril.chemistrykit.ChemistryKit;
+import aurril.chemistrykit.block.BuddingSulfurBlock;
+import aurril.chemistrykit.block.SulfurBlock;
+import aurril.chemistrykit.block.SulfurClusterBlock;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.*;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -14,6 +18,11 @@ public class ModBlocks {
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ModItems.CUSTOM_ITEM_GROUP_KEY).register((itemGroup) -> {
             itemGroup.add(ModBlocks.SULFUR_BLOCK.asItem());
+            itemGroup.add(ModBlocks.SMALL_SULFUR_BUD.asItem());
+            itemGroup.add(ModBlocks.MEDIUM_SULFUR_BUD.asItem());
+            itemGroup.add(ModBlocks.LARGE_SULFUR_BUD.asItem());
+            itemGroup.add(ModBlocks.SULFUR_CLUSTER.asItem());
+            itemGroup.add(ModBlocks.BUDDING_SULFUR_BLOCK.asItem());
         });
     }
 
@@ -32,13 +41,88 @@ public class ModBlocks {
     }
 
     public static final Block SULFUR_BLOCK = register(
-            new Block(
+            new SulfurBlock(
                     AbstractBlock.Settings.create()
+                            .mapColor(MapColor.EMERALD_GREEN)
                             .strength(1.5F)
                             .sounds(BlockSoundGroup.AMETHYST_BLOCK)
                             .requiresTool()
             ),
             "sulfur_block",
+            true
+    );
+
+    public static final Block SULFUR_CLUSTER = register(
+            new SulfurClusterBlock(
+                    7,
+                    3,
+                    AbstractBlock.Settings.create()
+                            .mapColor(MapColor.EMERALD_GREEN)
+                            .solid()
+                            .nonOpaque()
+                            .ticksRandomly()
+                            .sounds(BlockSoundGroup.AMETHYST_CLUSTER)
+                            .strength(1.5F)
+                            .luminance((state) -> 5)
+                            .pistonBehavior(PistonBehavior.DESTROY)
+            ),
+            "sulfur_cluster",
+            true
+    );
+
+    public static final Block SMALL_SULFUR_BUD = register(
+            new SulfurClusterBlock(
+                    3,
+                    4,
+                    AbstractBlock.Settings.copy(SULFUR_CLUSTER)
+                            .sounds(BlockSoundGroup.SMALL_AMETHYST_BUD)
+                            .solid()
+                            .luminance((state) -> 1)
+                            .pistonBehavior(PistonBehavior.DESTROY)
+            ),
+            "small_sulfur_bud",
+            true
+    );
+
+    public static final Block MEDIUM_SULFUR_BUD = register(
+            new SulfurClusterBlock(
+                    4,
+                    3,
+                    AbstractBlock.Settings.copy(SULFUR_CLUSTER)
+                            .sounds(BlockSoundGroup.MEDIUM_AMETHYST_BUD)
+                            .solid()
+                            .luminance((state) -> 4)
+                            .pistonBehavior(PistonBehavior.DESTROY)
+            ),
+            "medium_sulfur_bud",
+            true
+    );
+
+    public static final Block LARGE_SULFUR_BUD = register(
+            new SulfurClusterBlock(
+                    5,
+                    3,
+                    AbstractBlock.Settings.copy(SULFUR_CLUSTER)
+                            .sounds(BlockSoundGroup.LARGE_AMETHYST_BUD)
+                            .solid()
+                            .luminance((state) -> 2)
+                            .pistonBehavior(PistonBehavior.DESTROY)
+            ),
+            "large_sulfur_bud",
+            true
+    );
+
+    public static final Block BUDDING_SULFUR_BLOCK = register(
+            new BuddingSulfurBlock(
+                    AbstractBlock.Settings.create()
+                            .mapColor(MapColor.EMERALD_GREEN)
+                            .ticksRandomly()
+                            .strength(1.5F)
+                            .sounds(BlockSoundGroup.AMETHYST_BLOCK)
+                            .requiresTool()
+                            .pistonBehavior(PistonBehavior.DESTROY)
+            ),
+            "budding_sulfur",
             true
     );
 }
