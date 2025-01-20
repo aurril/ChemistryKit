b60fff529933c6b84671ffd7ce8f0d66f2c8b830
Silvio Wehner <silvio.wehner@googlemail.com>
2025-01-20 22:08:32 +0100

    Add data generation for: Models Recipes Tags

--- src/client/java/aurril/chemistrykit/ChemistryKitDataGenerator.java
+++ src/client/java/aurril/chemistrykit/ChemistryKitDataGenerator.java
@@ -1,6 +1,9 @@
 package aurril.chemistrykit;
 
 import aurril.chemistrykit.provider.loottable.ModBlocksLootTables;
+import aurril.chemistrykit.provider.model.ModModels;
+import aurril.chemistrykit.provider.recipe.ModRecipes;
+import aurril.chemistrykit.provider.tag.ModBlockTags;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -9,5 +12,8 @@ public class ChemistryKitDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(ModBlocksLootTables::new);
+		pack.addProvider(ModRecipes::new);
+		pack.addProvider(ModBlockTags::new);
+		pack.addProvider(ModModels::new);
 	}
 }
---
+++ src/client/java/aurril/chemistrykit/provider/tag/ModBlockTags.java
@@ -0,0 +1,26 @@
+package aurril.chemistrykit.provider.tag;
+
+import aurril.chemistrykit.registry.ModBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTags extends FabricTagProvider.BlockTagProvider {
+
+    public ModBlockTags(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        this.getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.SULFUR_BLOCK);
+
+        this.getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(ModBlocks.SULFUR_BLOCK)
+                .add(ModBlocks.BUDDING_SULFUR_BLOCK);
+    }
+}
